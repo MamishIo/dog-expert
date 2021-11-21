17bc7251dbe9133f74a4f1a07d8b2197353222e0
Hamish Tym <hamishtym@bigpond.com>
2021-11-22 01:10:48 +1100

    Use non-default VPC in Fargate service

--- dog-expert-cdk/src/main/java/io/mamish/dogexpert/cdk/ServiceStack.java
+++ dog-expert-cdk/src/main/java/io/mamish/dogexpert/cdk/ServiceStack.java
@@ -2,12 +2,18 @@ package io.mamish.dogexpert.cdk;
 
 import org.jetbrains.annotations.Nullable;
 import software.amazon.awscdk.core.Stack;
+import software.amazon.awscdk.services.ec2.SubnetConfiguration;
+import software.amazon.awscdk.services.ec2.SubnetType;
+import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.ec2.VpcProps;
 import software.amazon.awscdk.services.ecs.ContainerImage;
 import software.amazon.awscdk.services.ecs.patterns.QueueProcessingFargateService;
 import software.amazon.awscdk.services.ecs.patterns.QueueProcessingFargateServiceProps;
 import software.amazon.awscdk.services.secretsmanager.Secret;
 import software.amazon.awscdk.services.secretsmanager.SecretProps;
 import software.constructs.Construct;
+
+import java.util.List;
 
 public class ServiceStack extends Stack {
 
@@ -21,7 +27,19 @@ public class ServiceStack extends Stack {
                 .secretName("discord/api-token")
                 .build());
 
+        var serviceVpc = new Vpc(this, "DefaultVpc", VpcProps.builder()
+                .cidr("10.10.0.0/16")
+                .maxAzs(3)
+                .subnetConfiguration(List.of(
+                        SubnetConfiguration.builder()
+                                .name("Public")
+                                .subnetType(SubnetType.PUBLIC)
+                                .cidrMask(20)
+                                .build()))
+                .build());
+
         var discordFargateService = new QueueProcessingFargateService(this, "Service", QueueProcessingFargateServiceProps.builder()
+                .vpc(serviceVpc)
                 .cpu(CPU_UNITS)
                 .memoryLimitMiB(MEMORY_MB)
                 .image(ContainerImage.fromAsset("dog-expert-docker"))
