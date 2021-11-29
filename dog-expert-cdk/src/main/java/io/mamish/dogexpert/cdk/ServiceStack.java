8e176e7ddf6ea5a955f7df1c9df7f79b39fada6b
Hamish Tym <hamishtym@bigpond.com>
2021-11-30 01:10:33 +1100

    Use Fargate Spot

--- dog-expert-cdk/src/main/java/io/mamish/dogexpert/cdk/ServiceStack.java
+++ dog-expert-cdk/src/main/java/io/mamish/dogexpert/cdk/ServiceStack.java
@@ -1,16 +1,17 @@
 package io.mamish.dogexpert.cdk;
 
 import org.jetbrains.annotations.Nullable;
+import software.amazon.awscdk.core.RemovalPolicy;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.services.ec2.SubnetConfiguration;
 import software.amazon.awscdk.services.ec2.SubnetType;
 import software.amazon.awscdk.services.ec2.Vpc;
 import software.amazon.awscdk.services.ec2.VpcProps;
-import software.amazon.awscdk.services.ecs.ContainerImage;
-import software.amazon.awscdk.services.ecs.patterns.QueueProcessingFargateService;
-import software.amazon.awscdk.services.ecs.patterns.QueueProcessingFargateServiceProps;
+import software.amazon.awscdk.services.ecs.*;
+import software.amazon.awscdk.services.logs.LogGroup;
+import software.amazon.awscdk.services.logs.LogGroupProps;
+import software.amazon.awscdk.services.logs.RetentionDays;
 import software.amazon.awscdk.services.secretsmanager.Secret;
-import software.amazon.awscdk.services.secretsmanager.SecretProps;
 import software.constructs.Construct;
 
 import java.util.List;
@@ -26,23 +27,51 @@ public class ServiceStack extends Stack {
         var serviceVpc = new Vpc(this, "DefaultVpc", VpcProps.builder()
                 .cidr("10.10.0.0/16")
                 .maxAzs(3)
-                .subnetConfiguration(List.of(
-                        SubnetConfiguration.builder()
-                                .name("Public")
-                                .subnetType(SubnetType.PUBLIC)
-                                .cidrMask(20)
-                                .build()))
+                .subnetConfiguration(List.of(SubnetConfiguration.builder()
+                        .name("Public")
+                        .subnetType(SubnetType.PUBLIC)
+                        .cidrMask(20)
+                        .build()))
                 .build());
 
-        var discordFargateService = new QueueProcessingFargateService(this, "Service", QueueProcessingFargateServiceProps.builder()
+
+
+        var fargateCluster = new Cluster(this, "FargateCluster", ClusterProps.builder()
                 .vpc(serviceVpc)
-                .assignPublicIp(true)
+                .enableFargateCapacityProviders(true)
+                .containerInsights(true)
+                .build());
+        var task = new FargateTaskDefinition(this, "Task", FargateTaskDefinitionProps.builder()
                 .cpu(CPU_UNITS)
                 .memoryLimitMiB(MEMORY_MB)
+                .build());
+        var logGroup = new LogGroup(this, "ServiceLogs", LogGroupProps.builder()
+                .retention(RetentionDays.THREE_MONTHS)
+                .removalPolicy(RemovalPolicy.DESTROY)
+                .build());
+        var logDriver = LogDriver.awsLogs(AwsLogDriverProps.builder()
+                .logGroup(logGroup)
+                .streamPrefix("service")
+                .build());
+        task.addContainer("ServiceContainer", ContainerDefinitionOptions.builder()
                 .image(ContainerImage.fromAsset("dog-expert-docker"))
+                .logging(logDriver)
+                .build());
+        var service = new FargateService(this, "Service", FargateServiceProps.builder()
+                .cluster(fargateCluster)
+                .taskDefinition(task)
+                .capacityProviderStrategies(List.of(CapacityProviderStrategy.builder()
+                        .capacityProvider("FARGATE_SPOT")
+                        .weight(1)
+                        .build()))
+                .assignPublicIp(true)
+                .desiredCount(1)
+                .circuitBreaker(DeploymentCircuitBreaker.builder()
+                        .rollback(true)
+                        .build())
                 .build());
 
         var existingTokenSecret = Secret.fromSecretNameV2(this, "ImportedTokenSecret", "discord/api-token");
-        existingTokenSecret.grantRead(discordFargateService.getTaskDefinition().getTaskRole());
+        existingTokenSecret.grantRead(task.getTaskRole());
     }
 }
